035f30a0753e5a22160f9aaf603addd3c2398054
srohanshanker <70563583+srohanshanker@users.noreply.github.com>
2020-09-01 13:48:12 +0530

    Add files via upload

---
+++ SMS.UI/Controllers/StudentController.cs
@@ -0,0 +1,43 @@
+﻿using SMS.BusinessEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace SMS.UI.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            IEnumerable<StudentViewModel> students = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(ConfigurationManager.AppSettings["APIBaseURL"].ToString());
+                //HTTP GET
+                var responseTask = client.PostAsync("GetAllStudents", null);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<StudentViewModel>>();
+                    readTask.Wait();
+                    students = readTask.Result;
+                }
+                else //web api sent error response 
+                {
+                    students = Enumerable.Empty<StudentViewModel>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            return View(students);
+        }
+    }
+}
