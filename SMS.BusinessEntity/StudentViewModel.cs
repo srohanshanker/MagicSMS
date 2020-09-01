bb84b2ef0dfb6611e4856e5ac2b53813c87247c1
srohanshanker <70563583+srohanshanker@users.noreply.github.com>
2020-09-01 13:29:01 +0530

    Add files via upload

---
+++ SMS.BusinessEntity/StudentViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.BusinessEntity
+{
+    public class StudentViewModel
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public Nullable<System.DateTime> DateOfBirth { get; set; }
+        public string SSN { get; set; }
+        public string District { get; set; }
+        public string ServiceName { get; set; }
+        public string SchoolYear { get; set; }
+    }
+}
---
+++ SMS.BusinessService/Interface/IStudentService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SMS.BusinessEntity;
+using SMS.DAL;
+
+namespace SMS.BusinessService.Interface
+{
+    public interface IStudentService
+    {
+        List<StudentViewModel> GetAllStudents();
+        List<Student> GetAllStudents(int districtId);
+        List<Enrollment> GetAllStudentEnrollments(int studentId, string schoolYear);
+        List<DAL.Service> GetAllStudentServices(int studentId, string schoolYear);
+    }
+}
