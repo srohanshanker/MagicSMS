c0fa6f8d503c33f9d2dc6bf2e3055ebd63066fa0
srohanshanker <70563583+srohanshanker@users.noreply.github.com>
2020-09-01 13:34:20 +0530

    Add files via upload

---
+++ SMS.WebAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,31 @@
+﻿using SMS.BusinessService.Interface;
+using SMS.BusinessService.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Unity;
+using Unity.Lifetime;
+
+namespace SMS.WebAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // Web API configuration and services          
+            var container = new UnityContainer();
+            container.RegisterType<IStudentService, StudentService>(new HierarchicalLifetimeManager());
+            config.DependencyResolver = new DependencyResolver.UnityResolver(container);
+        }
+    }
+}
---
+++ SMS.WebAPI/Controllers/StudentController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SMS.BusinessEntity;
+using SMS.BusinessService.Interface;
+using SMS.BusinessService.Service;
+using SMS.DAL;
+
+namespace SMS.WebAPI.Controllers
+{
+    public class StudentController : ApiController
+    {
+        readonly IStudentService _studentService;
+        public StudentController(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
+        [HttpPost]
+        [Route("api/student/GetAllStudents")]
+        public List<StudentViewModel> GetAllStudents()
+        {
+            return _studentService.GetAllStudents();
+        }
+        [HttpPost]
+        [Route("api/student/GetAllStudents/{districtid}")]
+        public List<Student> GetAllStudents(int districtid)
+        {
+            return _studentService.GetAllStudents(districtid);
+        }
+        [HttpPost]
+        [Route("GetAllStudentEnrollments")]
+        public List<Enrollment> GetAllStudentEnrollments(int studentId, string schoolYear)
+        {
+            return _studentService.GetAllStudentEnrollments(studentId, schoolYear);
+        }
+        [HttpPost]
+        [Route("GetAllStudentServices")]
+        public List<Service> GetAllStudentServices(int studentId, string schoolYear)
+        {
+            return _studentService.GetAllStudentServices(studentId, schoolYear);
+        }
+    }
+}
